fe907f5505c40639c0c82c1eb515f39c78d5667a
Joéder Marques dos Santos <jhol.si@gmail.com>
2021-09-08 19:17:33 -0300

    Ajuste conversão ToDecimal

--- MontarPlanilha/Utils/StringExtension.cs
+++ MontarPlanilha/Utils/StringExtension.cs
@@ -8,20 +8,18 @@ namespace MontarPlanilha.Utils
         public static int ToInt(this string value, int linha)
         {
             if (int.TryParse(value.Trim(), out int idOut))
-                return  idOut;
+                return idOut;
             else
                 throw new Exception($"Erro na conversão de dados comuna {linha}. Verifique o formato do arquivo .csv !");
         }
 
         public static decimal ToDecimal(this string value, int linha)
         {
-            //var d = decimal.TryParse(value.Trim(), CultureInfo.InvariantCulture, out decimal outDecimal))
             value = value.Trim().Replace(" ", "");
-            //value = value.Replace(",", ".");
-            if (decimal.TryParse(value, out decimal outDecimal))
-                return outDecimal;
-            else
+            if (!decimal.TryParse(value, out decimal outDecimal))
                 throw new Exception($"Erro na conversão de dados comuna {linha}. Verifique o formato do arquivo .csv !");
+
+            return decimal.Parse(value, CultureInfo.InvariantCulture);
         }
     }
 }
